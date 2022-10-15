23234a8889236a0e5622865bcaf241f6ce4e3a28
Aadi-kashyav <101461343+Aadi-kashyav@users.noreply.github.com>
2022-10-15 19:05:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -28,3 +28,40 @@ function speak() {
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
 }
+function check() {
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult() {
+    if(error) {
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "Super") {
+            document.getElementById("update_emoji").innerHTML = "&#128512;";
+        }
+        if(results[0].label == "Thumbs up") {
+            document.getElementById("update_emoji").innerHTML = "&#128544;";
+        }
+        if(results[0].label == "Victory") {
+            document.getElementById("update_emoji").innerHTML = "&#128548;";
+        }
+
+        if(results[1].label == "Super") {
+            document.getElementById("update_emoji2").innerHTML = "&#128512;";
+        }
+        if(results[1].label == "Thumbs up") {
+            document.getElementById("update_emoji2").innerHTML = "&#128544;";
+        }
+        if(results[1].label == "Victory") {
+            document.getElementById("update_emoji2").innerHTML = "&#128548;";
+        }
+    }   
+}
